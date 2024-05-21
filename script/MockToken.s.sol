9b6b312584e4a8aa9a5c4ca50fac7f71cab08bb7
C9 <aiden@pingpong.build>
2024-05-21 16:49:58 +0800

    feat: add claim for multimint

--- script/MockToken.s.sol
+++ script/MockToken.s.sol
@@ -18,8 +18,8 @@ contract MockTokenScript is Script {
         tokens[1] = address(m2);
         MultiMint mm = new MultiMint(0xcE150D52d01B7cB0b0E1b37A52BCb2227f8E2D35, tokens);
 
-        mm.grantRole(MINTER_ROLE, 0x4a6A31787fcef281c426529061eD13aFFCD28724);
-        mm.grantRole(MINTER_ROLE, 0xcE150D52d01B7cB0b0E1b37A52BCb2227f8E2D35);
+//        mm.grantRole(MINTER_ROLE, 0x4a6A31787fcef281c426529061eD13aFFCD28724);
+//        mm.grantRole(MINTER_ROLE, 0xcE150D52d01B7cB0b0E1b37A52BCb2227f8E2D35);
         m1.grantRole(MINTER_ROLE, address(mm));
         m2.grantRole(MINTER_ROLE, address(mm));
 //        MockToken t0 = new MockToken("Mapped GRT Token", "GRT", 0xcE150D52d01B7cB0b0E1b37A52BCb2227f8E2D35);
--- src/MultiMint.sol
+++ src/MultiMint.sol
@@ -2,11 +2,26 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./interfaces/IMockToken.sol";
 
 contract MultiMint is AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    address public proverAddress = 0x4a6A31787fcef281c426529061eD13aFFCD28724;
+
+    uint256 public totalClaimedAmount;
+    mapping(address => uint256) public userClaimedAmount;
+    mapping(string => bool) public claimedIds;
+    uint256 public totalTxAmount;
+
     address[] public tokens;
+
+    /**
+   * @dev Thrown when the signer is not prover address
+     */
+    error WrongSignature(string id);
+
+    error RepeatedID(string id);
 
     constructor(address minter, address[] memory _tokens) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -20,15 +35,35 @@ contract MultiMint is AccessControl {
         tokens = _tokens;
     }
 
+    function setProverAddress(address _proverAddress) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        proverAddress = _proverAddress;
+    }
+
+    function claim(string memory id, uint256 amount, bytes memory proof) public {
+        if (claimedIds[id]) {
+            revert RepeatedID(id);
+        }
+
+        bytes32 signedHash = keccak256(abi.encodePacked(id, amount, msg.sender));
+
+        address signer = ECDSA.recover(signedHash, proof);
+        if (signer != proverAddress) {
+            revert WrongSignature(id);
+        }
+
+        for (uint i = 0; i < tokens.length; i++) {
+            IMockToken(tokens[i]).mint(msg.sender, amount);
+        }
+
+        userClaimedAmount[msg.sender] += amount;
+        totalClaimedAmount += amount;
+        claimedIds[id] = true;
+        totalTxAmount += 1;
+    }
+
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         for (uint i = 0; i < tokens.length; i++) {
             IMockToken(tokens[i]).mint(to, amount);
-        }
-    }
-
-    function mintWithId(address to, uint256 amount, string memory id) public onlyRole(MINTER_ROLE) {
-        for (uint i = 0; i < tokens.length; i++) {
-            IMockToken(tokens[i]).mintWithId(to, amount, id);
         }
     }
 }
