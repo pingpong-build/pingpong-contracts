972b3ac9b6ca46a779fd678b6037c01413bc4672
C9 <aiden@pingpong.build>
2024-09-05 12:41:36 +0800

    feat(mpf): update script

--- script/MiningPassFactory.s.sol
+++ script/MiningPassFactory.s.sol
@@ -12,7 +12,7 @@ contract MiningPassFactoryScript is Script, Constants {
     uint256 chainId = uint256(vm.envUint("CHAIN_ID"));
     address usdt;
     address collector;
-    address mpfAddress = 0xC3f1823dC102d95E063e8d226331356086945236;
+    address mpfAddress = 0x793C94C1e45f9Bb5801E08898Ff16743a8561BcD;
 
     function setUp() public {
         console2.log("CHAIN_ID: ", chainId);
@@ -29,7 +29,7 @@ contract MiningPassFactoryScript is Script, Constants {
         vm.startBroadcast();
 
         MiningPassFactory mpf = new MiningPassFactory(usdt, collector, "https://resource.pingpong.build/static/future/");
-        mpf.createRound(0, 1000, 0.0001 * 1000_000, block.timestamp, block.timestamp + 7 * 24 * 3600, block.timestamp + 0 * 24 * 3600, 30);
+        mpf.createRound(1, 2500, 40 * 1000_000, block.timestamp, block.timestamp + 30 * 24 * 3600, block.timestamp, 30);
 
         vm.stopBroadcast();
     }
@@ -50,7 +50,18 @@ contract MiningPassFactoryScript is Script, Constants {
         vm.startBroadcast();
 
         MiningPassFactory mpf = MiningPassFactory(mpfAddress);
-        mpf.grantRole(mpf.OPERATOR_ROLE(), 0x0513b9F7F42666622672d6a52c1E62A76ccC7AB0);
+        mpf.grantRole(mpf.DEFAULT_ADMIN_ROLE(), 0xB90C7395D52e8af26773EB89250DbEaAAc107c01);
+//        mpf.grantRole(mpf.OPERATOR_ROLE(), 0xB90C7395D52e8af26773EB89250DbEaAAc107c01);
+
+        vm.stopBroadcast();
+    }
+
+    function revoke() public {
+        vm.startBroadcast();
+
+        MiningPassFactory mpf = MiningPassFactory(mpfAddress);
+        mpf.revokeRole(mpf.OPERATOR_ROLE(), 0x3212dBfCA59b74F5eD208b64f171a0C8BcF2e8C0);
+        mpf.revokeRole(mpf.DEFAULT_ADMIN_ROLE(), 0x3212dBfCA59b74F5eD208b64f171a0C8BcF2e8C0);
 
         vm.stopBroadcast();
     }
--- script/constants.sol
+++ script/constants.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.0;
 contract Constants {
     address public constant polygonUsdt = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
     address public constant amoyUsdt = 0x78137Bb8588D1a8942E73C62881482b1dc650F40;
-    address public constant polygonCollector = 0xe5eDa14169623491D7fF5fBb8AC76a39b677Ad4E;
+    address public constant polygonCollector = 0x5C190758a9742B221039f35A45824351542BFF37;
     address public constant amoyCollector = 0xBfE5E7792A91d6f91626d0F08c8052702c9b9c51;
 }
